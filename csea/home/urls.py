c25f0376b3f4cb875188d8ffce1e3170645d8cda
khiladimahan1210 <gupta18e@iitg.ac.in>
2019-10-03 18:32:34 +0530

    added members page

--- csea/home/urls.py
+++ csea/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'home'
 
 urlpatterns = [
-    
+    path('members/',TemplateView.as_view(template_name='members.html')),
 ]
