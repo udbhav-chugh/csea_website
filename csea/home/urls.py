f0756fce9d1ce61026ee320395bc72c326ca2787
khiladimahan1210 <gupta18e@iitg.ac.in>
2019-10-07 16:35:25 +0530

    made base.html and events page

--- csea/home/urls.py
+++ csea/home/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 app_name = 'home'
 
 urlpatterns = [
-    path('members/',TemplateView.as_view(template_name='members.html')),
+    path('members/',TemplateView.as_view(template_name='members1.html')),
+    path('events/',TemplateView.as_view(template_name='events1.html')),
     path('', TemplateView.as_view(template_name="home.html")),
 ]
